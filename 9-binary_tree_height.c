766b14732ebaa1ab27c0a692b6f1d3ed9edd6ea5
LennyIsMyName <lennyofficial2001@gmail.com>
2023-06-02 14:57:32 +0300

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,17 +8,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t leftHeight, rightHeight;
+
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		size_t leftHeight = binary_tree_height(tree->left);
-		size_t rightHeight = binary_tree_height(tree->right);
 
-		if (leftHeight >= rightHeight)
-			return (leftHeight + 1);
-		else
-			return (rightHeight + 1);
-	}
+	leftHeight = binary_tree_height(tree->left);
+	rightHeight = binary_tree_height(tree->right);
+
+	if (leftHeight >= rightHeight)
+		return (leftHeight + 1);
+	else
+		return (rightHeight + 1);
 }
 
