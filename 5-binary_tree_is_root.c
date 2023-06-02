a659910ccea2716c8962f8b2a9c6c99baac0ad3b
LennyIsMyName <lennyofficial2001@gmail.com>
2023-06-02 09:54:38 +0300

    pre-order traversal

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if the given node is a root.
+ * @node: the node to check.
+ * Return: 1 if node is a root or else 0.
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent != NULL)
+		return (0);
+	else
+		return (1);
+}	
