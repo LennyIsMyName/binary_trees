a6494fd01c4af9bb2992cdb95261dc90201215b1
LennyIsMyName <lennyofficial2001@gmail.com>
2023-06-03 02:41:15 +0300

    sibblings

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,10 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	size_t leftHeight = binary_tree_height(tree->left);
 	size_t rightHeight = binary_tree_height(tree->right);
 
-	return ((leftHeight > rightHeight ? leftHeight : rightHeight) + 1);
+	return ((leftHeight > rightHeight) ? (leftHeight + 1) : (rightHeight + 1));
 }
